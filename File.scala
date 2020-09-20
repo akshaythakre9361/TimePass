142e294c9b66f1818eac2cde18cc68ebfc9e5d84
akshaythakre9361 <52557444+akshaythakre9361@users.noreply.github.com>
2020-09-20 15:08:38 +0530

    Create File.scala
    First Commit

---
+++ File.scala
@@ -0,0 +1,16 @@
+class demoFunction {
+
+  def add(a: Int, b: Int): Int = {
+    var sum: Int = 0
+    sum = a + b
+    return sum
+  }
+
+}
+object MainObject {
+  def main(args: Array[String]): Unit = {
+    var s = new demoFunction
+    var res = s.add(10,20)
+    println("Addition : " + res)
+  }
+}
